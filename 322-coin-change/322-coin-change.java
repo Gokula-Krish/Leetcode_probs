b353fefbd83b89938401ab4204f2dc3a8563a4c4
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-02-19 12:57:01 +0530

    Time: 164 ms (9.56%), Space: 42.8 MB (40.41%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,35 @@
+class Solution {
+    // int min=Integer.MAX_VALUE;
+    HashMap<Integer,Integer> map=new HashMap();
+    public int findCoins(int[] coins,int s){
+        if(s<0){
+            return -1;
+        }
+        if(s==0){
+            return 0;
+        }
+         if(map.containsKey(s)){
+            return map.get(s);
+        }
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<coins.length;i++){
+            int m=findCoins(coins,s-coins[i]);
+            if(m>=0 && m<min){
+                min=1+m;
+            }
+        }
+        if(min==Integer.MAX_VALUE){
+            map.put(s,-1);
+        }
+        else{
+            map.put(s,min);
+        }
+        return map.get(s);
+    }
+    public int coinChange(int[] coins, int amount) {
+       // counts=new int[amount+1];
+        int ans=findCoins(coins,amount);
+        return ans;
+    }
+    // int[] counts;
+}
