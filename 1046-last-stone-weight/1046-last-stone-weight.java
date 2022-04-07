806f37c4071a4011eecc03e967ba8f70a7336f62
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-04-07 05:39:11 +0530

    Time: 3 ms (35.54%), Space: 41.5 MB (55.02%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int x:stones){
+            pq.add(x);
+        }
+        while(pq.size()>1){
+            int x=pq.poll();
+            int y=pq.poll();
+            if(x==y){}
+            else if(x!=y){
+                int z=Math.abs(y-x);
+                pq.add(z);
+            }
+        }
+        if(!pq.isEmpty()){
+            return pq.poll();
+        }
+        return 0;
+    }
+}
