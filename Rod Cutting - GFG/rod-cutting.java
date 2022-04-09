01036b61af3d8f154a206a8f2adac81502b9d04a
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-04-09 13:05:56 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.java
+++ Rod Cutting - GFG/rod-cutting.java
@@ -24,30 +24,19 @@ class Solution{
     public int cutRod(int price[], int n) {
         //code here
         int[][] dp=new int[n+1][n+1];
-        for(int[] d:dp){
-            Arrays.fill(d,-1);
-        }
-        int[] len=new int[n];
         for(int i=1;i<=n;i++){
-            len[i-1]=i;
+            for(int j=1;j<=n;j++){
+                if(i<j){
+                    dp[i][j]=Math.max(dp[i][j-i]+price[i-1],dp[i-1][j]);
+                }
+                else if(i==j){
+                    dp[i][j]=Math.max(price[i-1],dp[i-1][j]);
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
         }
-        int max=n;
-        return rodCut(price,len,n,max,dp); 
-    }
-    public int rodCut(int[] price,int[] len,int n,int L,int[][] dp){
-        if(n==0 || L==0){
-            return 0;
-        }
-        int res;
-        if(dp[n][L]!=-1){
-            return dp[n][L];
-        }
-        if(len[n-1]>L){
-            dp[n][L]=rodCut(price,len,n-1,L,dp);
-        }
-        else{
-            dp[n][L]=Math.max(rodCut(price,len,n-1,L,dp),price[n-1]+rodCut(price,len,n,L-len[n-1],dp));
-        }
-        return dp[n][L];
+        return dp[n][n];
     }
 }
