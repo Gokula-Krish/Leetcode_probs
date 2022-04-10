8d5cf5e524af553adfa532ee51061b703507bdbd
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-04-10 13:04:15 +0530

    Time: 13 ms (5.89%), Space: 45.4 MB (5.24%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isInt(String s){
+        try{
+            int a=Integer.parseInt(s);
+        }
+        catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+    public int calPoints(String[] ops) {
+        int total=0;
+        Stack<Integer> stack=new Stack();
+        for(String s:ops){
+            if(isInt(s)){
+                stack.push(Integer.parseInt(s));
+            }
+            else if(s.equals("C")){
+                stack.pop();
+            }
+            else if(s.equals("D")){
+                int a=stack.peek();
+                stack.push(2*a);
+            }
+            else{
+                int a=stack.pop();
+                int b=stack.pop();
+                int c=a+b;
+                stack.push(b);
+                stack.push(a);
+                stack.push(c);
+            }
+        }
+        for(int a:stack){
+            total+=a;
+        }
+        return total;
+    }
+}
