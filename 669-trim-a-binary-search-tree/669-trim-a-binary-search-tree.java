49283e281204561100e97d3c824b2d0bfdcee9a8
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-04-15 07:12:01 +0530

    Time: 0 ms (100.00%), Space: 42 MB (84.81%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
@@ -18,10 +18,10 @@ class Solution {
         if(root==null){
             return root;
         }
-        if(root.val<low){
+        else if(root.val<low){
             return trimBST(root.right,low,high);
         }
-        if(root.val>high){
+        else if(root.val>high){
             return trimBST(root.left,low,high);
         }
         root.left=trimBST(root.left,low,high);
