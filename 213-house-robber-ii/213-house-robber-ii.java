db647ec704491a79e6e778621c7f9a2fbf97c76a
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-04-24 16:04:10 +0530

    Time: 1 ms (28.39%), Space: 41.6 MB (42.37%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,28 +1,18 @@
 class Solution {
+    public int houseRob1(int[] nums){
+        int rob1=0,rob2=0,t;
+        for(int i:nums){
+            t=Math.max(i+rob1,rob2);
+            rob1=rob2;
+            rob2=t;
+        }
+        return rob2;
+    }
     public int rob(int[] nums) {
         int n=nums.length;
         if(n==1){
             return nums[0];
         }
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        int a=nonAdjacentSum(1,n-1,nums,dp);
-        Arrays.fill(dp,-1);
-        int b=nonAdjacentSum(0,n-2,nums,dp);
-        return Math.max(a,b);
-    }
-    public int nonAdjacentSum(int start,int i,int[] nums,int[] dp){
-        if(i==start){
-            return nums[i];
-        }
-        if(i<start){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int a=nums[i]+nonAdjacentSum(start,i-2,nums,dp);
-        int b=nonAdjacentSum(start,i-1,nums,dp);
-        return dp[i]=Math.max(a,b);
+        return Math.max(houseRob1(Arrays.copyOfRange(nums,0,n-1)),houseRob1(Arrays.copyOfRange(nums,1,n)));
     }
 }
