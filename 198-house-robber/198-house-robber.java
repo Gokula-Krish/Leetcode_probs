62ff0b1907af13a0518d352fa3e6582603800fd1
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-04-24 15:58:32 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (27.06%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,22 +1,11 @@
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        return nonAdjacentSum(0,nums,n-1,dp);
-    }
-    public int nonAdjacentSum(int start,int[] nums,int i,int[] dp){
-        if(i==start){
-            return nums[i];
+        int rob1=0,rob2=0,t;
+        for(int i:nums){
+            t=Math.max(i+rob1,rob2);
+            rob1=rob2;
+            rob2=t;
         }
-        if(i<0){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int a=nums[i]+nonAdjacentSum(start,nums,i-2,dp);
-        int b=nonAdjacentSum(start,nums,i-1,dp);
-        return dp[i]=Math.max(a,b);
+        return rob2;
     }
 }
