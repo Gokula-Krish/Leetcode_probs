eaec1ea1eb86f70575d2688c753be383022074e1
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-05-03 08:06:36 +0530

    Time: 9 ms (34.10%), Space: 54.2 MB (36.81%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -4,23 +4,20 @@ class Solution {
         Arrays.sort(arr);
         int l=0,r=nums.length-1;
         boolean left=false,right=false;
+        int s=-1,e=-1;
         while(l<r){
-            if(nums[l]!=arr[l]){
-                left=true;
+            if(nums[l]!=arr[l] && s==-1){
+                s=l;
             }
-            if(nums[r]!=arr[r]){
-                right=true;
-            }
-            if(!left){
-                l++;
-            }
-            if(!right){
-                r--;
-            }
-            if(left && right){
+            if(nums[r]!=arr[r] && e==-1){
+                e=r;
+            }   
+            if(s!=-1 && e!=-1){
                 break;
             }
+            l=(s==-1)?++l:l;
+            r=(e==-1)?--r:r;
         }
-        return r-l==0?0:(r-l)+1;
+        return e-s<=0?0:(e-s)+1;
     }
 }
