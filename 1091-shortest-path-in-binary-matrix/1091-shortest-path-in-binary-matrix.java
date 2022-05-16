6cdf47a7342b2bd7eaba0ee8f209ba9753f89cbd
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-05-16 08:47:46 +0530

    Time: 25 ms (54.31%), Space: 61.1 MB (30.77%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(int[][] grid,boolean[][] visited,int r,int c,int n){    
+        return (r>=0 && c>=0 && r<n && c<n && !visited[r][c] && grid[r][c]==0);
+    }
+    int[] Row={-1,1,0,0,-1,-1,1,1};
+    int[] Col={0,0,-1,1,1,-1,1,-1};
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n=grid.length;
+        if(grid[0][0]==1){
+            return -1;
+        }
+        boolean[][] visited=new boolean[n][n];
+        Queue<int[]> q=new LinkedList();
+        q.add(new int[]{0,0,1});
+        visited[0][0]=true;
+        while(!q.isEmpty()){
+            int[] pair=q.poll();
+            if(pair[0]==n-1 && pair[1]==n-1){
+                return pair[2];
+            }
+            for(int i=0;i<8;i++){
+                int pX=pair[0]+Row[i];
+                int pY=pair[1]+Col[i];
+                if(isValid(grid,visited,pX,pY,n)){     
+                    q.add(new int[]{pX,pY,pair[2]+1});
+                    visited[pX][pY]=true;
+                }
+            }
+        }
+        return -1;
+    }
+}
