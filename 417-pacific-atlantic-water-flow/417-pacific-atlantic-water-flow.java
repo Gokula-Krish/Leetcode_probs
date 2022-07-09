c09c7d763a33374ee6e1b3d2a981683572801da7
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-07-09 18:03:05 +0530

    Time: 158 ms (9.02%), Space: 106.4 MB (5.50%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -2,9 +2,19 @@ class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         int m=heights.length,n=heights[0].length;
         List<List<Integer>> ans=new ArrayList(n);
+        boolean[][] pacific=new boolean[m][n];
+        boolean[][] atlantic=new boolean[m][n];
+        for(int i=0;i<n;i++){
+            bfs(heights,0,i,pacific);
+            bfs(heights,m-1,i,atlantic);
+        }
+        for(int i=0;i<m;i++){
+            bfs(heights,i,0,pacific);
+            bfs(heights,i,n-1,atlantic);
+        }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(bfs(heights,i,j)){
+                if(pacific[i][j] && atlantic[i][j]){
                     List<Integer> l=new ArrayList();
                     l.add(i);
                     l.add(j);
@@ -14,42 +24,28 @@ class Solution {
         }
         return ans;
     }
-    int[] row={-1,0,0,1};
-    int[] col={0,1,-1,0};
-    boolean pac=false;
-    boolean atl=false;
-    public boolean bfs(int[][] A,int r,int c){
-        pac=false;
-        atl=false;
+    int[] row={0,0,1,-1};
+    int[] col={1,-1,0,0};
+    public void bfs(int[][] A,int r,int c,boolean[][] ocean){
         Queue<int[]> q=new LinkedList();
         boolean[][] visited=new boolean[A.length][A[0].length];
         q.add(new int[]{r,c});
+        visited[r][c]=true;
+        ocean[r][c]=true;
         while(!q.isEmpty()){
             int[] pair=q.poll();
             for(int i=0;i<4;i++){
                 int pX=pair[0]+row[i];
                 int pY=pair[1]+col[i];
-                if(c==A[0].length || r==A.length){
-                    pac=true;
-                }
-                if(c==-1 || r==-1){
-                    
-                }
                 if(isValid(pair,pX,pY,visited,A)){
                     q.add(new int[]{pX,pY});
+                    ocean[pX][pY]=true;
                     visited[pX][pY]=true;
                 }
             }
         }
-        return pac && atl;
     }
     public boolean isValid(int[] pair,int r,int c,boolean[][] visited,int[][] A){
-        if(c==A[0].length || r==A.length){
-            pac=true;
-        }
-        if(c==-1 || r==-1){
-            atl=true;
-        }
-        return (r>=0 && c>=0 && r<A.length && c<A[0].length && !visited[r][c] && A[pair[0]][pair[1]]>=A[r][c]);
+        return (r>=0 && c>=0 && r<A.length && c<A[0].length && !visited[r][c] && A[pair[0]][pair[1]]<=A[r][c]);
     }
 }
