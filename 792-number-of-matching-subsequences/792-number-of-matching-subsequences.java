3653d3eb879bb7bf6535fb2fcc9f71d4fc15135b
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-07-20 22:19:20 +0530

    Time: 126 ms (72.50%), Space: 43 MB (98.66%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -5,11 +5,12 @@ class Solution {
         for(String str:words){
             map.put(str,map.getOrDefault(str,0)+1);
         }
+        char[] arr=s.toCharArray();
         for(Map.Entry<String,Integer> e:map.entrySet()){
             String str=e.getKey();
             int k=0,j=0;
             for(int i=0;i<s.length() && j<str.length();i++){
-                if(s.charAt(i)==str.charAt(j)){
+                if(arr[i]==str.charAt(j)){
                     j++;k++;
                 }
             }
