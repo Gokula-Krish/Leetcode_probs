2a5bb57a76692d76167e85c6f900786d3930b552
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-07-25 22:01:58 +0530

    Time: 13 ms (30.52%), Space: 58.6 MB (76.72%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -6,15 +6,11 @@ class Solution {
         stack.push(start);
         while(!stack.empty()){
             int top=stack.pop();
-             if(visited[top]){
-                return false;
-            }
             if(arr[top]==0){
                 return true;
             }
             int s=top-arr[top];
             int e=top+arr[top];
-           
             visited[top]=true;
             if(s>=0 && !visited[s]){
                 stack.push(s);
