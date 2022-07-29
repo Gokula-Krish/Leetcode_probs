f916da554030ac2c8f3fe1d4d1fc1c0a04324687
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-07-29 18:05:43 +0530

    Time: 51 ms (6.34%), Space: 42.7 MB (77.63%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -6,22 +6,12 @@ class Solution {
         LinkedHashMap<Character,Character> lh=new LinkedHashMap<Character,Character>();
         LinkedHashMap<Character,Character> lh1=new LinkedHashMap<Character,Character>();
         for(int i=0;i<s.length();i++){
-            if(lh.containsKey(s.charAt(i))){
-                if(lh.get(s.charAt(i))!=t.charAt(i)){
-                    return false;
-                }
+            if((lh1.containsKey(t.charAt(i)) && lh1.get(t.charAt(i))!=s.charAt(i))
+              || (lh.containsKey(s.charAt(i)) && lh.get(s.charAt(i))!=t.charAt(i))){
+                return false;
             }
             else{
                 lh.put(s.charAt(i),t.charAt(i));
-            }
-        }
-        for(int i=0;i<t.length();i++){
-            if(lh1.containsKey(t.charAt(i))){
-                if(lh1.get(t.charAt(i))!=s.charAt(i)){
-                    return false;
-                }
-            }
-            else{
                 lh1.put(t.charAt(i),s.charAt(i));
             }
         }
