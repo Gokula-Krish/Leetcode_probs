a95dba75cb2c4f517a176c2db727314acf5fff38
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-07-29 22:09:47 +0530

    Time: 3 ms (54.32%), Space: 43.7 MB (10.63%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.java
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -3,16 +3,16 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        HashMap<Character,Character> lh=new HashMap<Character,Character>();
-        HashMap<Character,Character> lh1=new HashMap<Character,Character>();
+        HashMap<Character,Character> map=new HashMap<Character,Character>();
+        HashMap<Character,Character> map1=new HashMap<Character,Character>();
         for(int i=0;i<s.length();i++){
-            if((lh1.containsKey(t.charAt(i)) && lh1.get(t.charAt(i))!=s.charAt(i))
-              || (lh.containsKey(s.charAt(i)) && lh.get(s.charAt(i))!=t.charAt(i))){
+            if((map1.containsKey(t.charAt(i)) && map1.get(t.charAt(i))!=s.charAt(i))
+              || (map.containsKey(s.charAt(i)) && map.get(s.charAt(i))!=t.charAt(i))){
                 return false;
             }
             else{
-                lh.put(s.charAt(i),t.charAt(i));
-                lh1.put(t.charAt(i),s.charAt(i));
+                map.put(s.charAt(i),t.charAt(i));
+                map1.put(t.charAt(i),s.charAt(i));
             }
         }
         return true;
