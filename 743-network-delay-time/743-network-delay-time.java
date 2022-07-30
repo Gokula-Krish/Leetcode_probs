b5b0694fa573e17cdf8a02e0787b892ddd27da16
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-07-30 16:02:53 +0530

    Time: 27 ms (58.98%), Space: 61.5 MB (79.53%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.java
@@ -0,0 +1,32 @@
+class Solution {
+    int[] dist;
+    public int networkDelayTime(int[][] times, int n, int k) {
+        bellman_ford(n,times,k);
+        int max=Integer.MIN_VALUE;
+       // System.out.println(Arrays.toString(dist));
+        for(int i=1;i<=n;i++){
+            if(dist[i]!=100000000 && i!=k){
+                max=Math.max(max,dist[i]);   
+            }
+            else if(dist[i]==100000000){
+                return -1;
+            }
+        }
+        return max==Integer.MIN_VALUE?-1:max;
+    }
+    int[] bellman_ford(int V,int[][] A, int S){
+        // Write your code here
+         dist=new int[V+1];
+         Arrays.fill(dist,100000000);
+         dist[S]=0;
+         for(int i=1;i<=V;i++){
+             for(int[] l:A){
+                 int u=l[0],v=l[1],cost=l[2];
+                 if(dist[u]+cost<dist[v]){
+                     dist[v]=dist[u]+cost;
+                 }
+             }
+         }
+         return dist;
+    }
+}
