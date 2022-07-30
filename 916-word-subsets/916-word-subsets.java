cca6f0ebb8314dbc9b964a13c92ee5b5d5e4856b
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-07-30 06:55:30 +0530

    Time: 28 ms (81.30%), Space: 88 MB (61.38%) - LeetHub

--- 916-word-subsets/916-word-subsets.java
+++ 916-word-subsets/916-word-subsets.java
@@ -5,23 +5,21 @@ class Solution {
             count1[c-'a']++;
         }
         for(int i=0;i<26;i++){
-                if(count[i]<=count1[i]){
-                    continue;
-                }
-                else{
-                    return false;
-                }
+            if(count[i]<=count1[i]){
+                continue;
+            }
+            else{
+                return false;
+            }
         }
         return true;
     }
     public List<String> wordSubsets(String[] words1, String[] words2) {
         ArrayList<String> ans=new ArrayList();
-        
         int[] map=new int[26];
         for(String s:words2){
             int[] count=new int[26];
             for(char c:s.toCharArray()){
-                
                 count[c-'a']++;
             }
             for(int i=0;i<26;i++){
