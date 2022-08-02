0a9071642ab81ee17ea24c2ba0e76b41192711a3
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-02 23:31:02 +0530

    Time: 27 ms (36.21%), Space: 72.7 MB (48.64%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        Queue<int[]> q=new LinkedList();
+        int zero=0;
+        int[] r={1,0,-1,0};
+        int[] c={0,1,0,-1};
+        boolean[][] visited=new boolean[mat.length][mat[0].length];
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[i].length;j++){
+                if(mat[i][j]==0){
+                    q.add(new int[]{i,j});
+                }
+            }
+        }
+     //   boolean[][] visited=new boolean[mat.length][mat[0].length];
+      //  int i=100;
+        while(!q.isEmpty()){
+          //  System.out.println();
+            int n=q.size();
+            while(n-->0){
+            int[] pair=q.poll();
+           // if(visited[pair[0]][pair[1]]){continue;}
+            for(int i=0;i<4;i++){
+                int pX=pair[0]+r[i];
+                int pY=pair[1]+c[i];
+              //  System.out.println(pX+",,,,,,"+pY);
+                if(isValid(pX,pY,mat) && (mat[pX][pY]==1)){
+                    if(visited[pX][pY]){
+                        mat[pX][pY]=Math.min(mat[pX][pY],mat[pair[0]][pair[1]]+1);
+                    }
+                    else{
+                        mat[pX][pY]=mat[pair[0]][pair[1]]+1;
+                       // System.out.println(mat[pX][pY]+"----"+pX+","+pY);
+                        visited[pX][pY]=true;
+                        q.add(new int[]{pX,pY});
+                    }
+                    
+                }
+            }}
+        }
+        return mat;
+    }
+    public boolean isValid(int r,int c,int[][] mat){
+        return r>=0 && c>=0 && r<mat.length && c<mat[0].length;
+    }
+}
