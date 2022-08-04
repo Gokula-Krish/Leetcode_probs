2688027c7cfd0c035e6a879df48a2d26234272db
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-04 18:44:33 +0530

    Time: 12 ms (19.93%), Space: 43.8 MB (68.18%) - LeetHub

--- 331-verify-preorder-serialization-of-a-binary-tree/331-verify-preorder-serialization-of-a-binary-tree.java
+++ 331-verify-preorder-serialization-of-a-binary-tree/331-verify-preorder-serialization-of-a-binary-tree.java
@@ -6,8 +6,8 @@ class Solution {
         }
         Stack<String> stack=new Stack();
         for(int i=0;i<sp.length;i++){
-            if(sp[i].equals("#")){
-                while(stack.size()>=2 && stack.peek().equals("#")){
+            if(sp[i].charAt(0)=='#'){
+                while(stack.size()>=2 && stack.peek().charAt(0)=='#'){
                     stack.pop();
                     if(stack.peek().charAt(0)!='#')
                         stack.pop();
@@ -16,6 +16,6 @@ class Solution {
             stack.push(sp[i]);
             
         }
-        return stack.size()==1 && stack.peek().equals("#");
+        return stack.size()==1 && stack.peek().charAt(0)=='#';
     }
 }
