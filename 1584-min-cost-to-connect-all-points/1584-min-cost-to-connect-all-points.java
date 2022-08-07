efc17a4fc59956fb6fb3c5fd4a3923fa5b7b86e0
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-07 23:51:10 +0530

    Time: 670 ms (38.81%), Space: 203.2 MB (7.47%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        LinkedList<int[]> graph[]=new LinkedList[points.length];
+        for(int i=0;i<points.length;i++){
+            graph[i]=new LinkedList();
+        }
+        for(int i=0;i<points.length;i++){
+            int[] a=points[i];
+            for(int j=i+1;j<points.length;j++){
+                int diff=Math.abs(a[0]-points[j][0])+Math.abs(a[1]-points[j][1]);
+                graph[i].add(new int[]{j,diff});
+                graph[j].add(new int[]{i,diff});
+            }
+        }
+        int cost=0;
+        PriorityQueue<int[]> pq=new PriorityQueue(new Comparator<int[]>(){
+           @Override
+            public int compare(int[] a,int[] b){
+                return a[1]-b[1];
+            }
+        });
+        HashSet<Integer> visit=new HashSet();
+        pq.add(new int[]{0,0});
+        while(visit.size()<points.length){
+            int[] edg=pq.poll();
+            if(visit.contains(edg[0])){
+                continue;
+            }
+            visit.add(edg[0]);
+            cost+=edg[1];
+            for(int[] arr:graph[edg[0]]){
+                if(!visit.contains(arr[0])){
+                    pq.add(arr);
+                }
+            }
+        }
+        return cost;
+    }
+}
