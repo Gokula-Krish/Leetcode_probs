8c5f2a6b9e6a43b87f16a4e70600d15e8eb5d5bc
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-08 21:32:37 +0530

    Time: 620 ms (7.55%), Space: 243.4 MB (5.02%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -3,20 +3,20 @@ class Solution {
         if(j==nums.length){
             return 0;
         }
-        if(dp[prev+1][j]!=-1){
-          //  System.out.println(prev+"----"+j+"---"+dp[prev+1][j]);
-            return dp[prev+1][j];
+        if(prev!=-1 && dp[prev][j]!=-1){
+            return dp[prev][j];
         }
         int len=0;
         if(prev==-1 || nums[j]>nums[prev]){
             len=1+Math.max(len,LIS(nums,j+1,j,dp));
         }
         len=Math.max(len,LIS(nums,j+1,prev,dp));
-        dp[prev+1][j]=len;
+        if(prev!=-1)
+            dp[prev][j]=len;
         return len;
     }
     public int lengthOfLIS(int[] nums) {
-        int[][] dp=new int[nums.length][nums.length];
+        int[][] dp=new int[nums.length+1][nums.length];
         for(int[] d:dp){
             Arrays.fill(d,-1);
         }
