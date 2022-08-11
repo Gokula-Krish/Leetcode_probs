f5dc2e46d6e096ab740cab8f4f97cd832c125da7
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-11 23:35:07 +0530

    Time: 19 ms (14.20%), Space: 45.8 MB (7.58%) - LeetHub

--- 71-simplify-path/71-simplify-path.java
+++ 71-simplify-path/71-simplify-path.java
@@ -5,11 +5,13 @@ class Solution {
     public String simplifyPath(String path) {
         Stack<String> stack=new Stack();
         StringBuilder sb=new StringBuilder();
+        int n=0;
         for(char c:path.toCharArray()){
             if(c!='/' && (Character.isAlphabetic(c) || isAny(c))){
                 sb.append(c);
+                n++;
             }
-            else if(c=='/' && sb.length()>0){
+            else if(c=='/' && n>0){
                 if(sb.toString().equals("..")){
                     if(!stack.empty())
                         stack.pop();
@@ -18,6 +20,7 @@ class Solution {
                     stack.push(sb.toString());
                 }
                 sb=new StringBuilder();
+                n=0;
             }
         }
         if(!sb.isEmpty() && sb.toString().equals("..")){
