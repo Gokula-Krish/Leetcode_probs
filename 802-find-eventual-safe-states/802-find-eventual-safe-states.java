c032bc166e691c81a1eb9dcab4ca54d4f3057f3c
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-11 21:00:10 +0530

    Time: 9 ms (58.45%), Space: 71.2 MB (38.71%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.java
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.java
@@ -1,27 +1,26 @@
 class Solution {
     ArrayList<Integer> ls=new ArrayList();
-   public boolean dfs(int[][] adj,int v,boolean[] visited,boolean[] reStack){
-        if(reStack[v]){
+   public boolean dfs(int[][] adj,int v,boolean[] visited,boolean[] dfsVisit){
+        if(dfsVisit[v]){
             return true;
         }
        if(visited[v]){
            return false;
        }
         visited[v]=true;
-        reStack[v]=true;
+        dfsVisit[v]=true;
         for(int i:adj[v]){
-            if(dfs(adj,i,visited,reStack)){
+            if(dfs(adj,i,visited,dfsVisit)){
                 return true;
             }
         }
-        reStack[v]=false;
+        dfsVisit[v]=false;
         return false;
     }
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int i=0,n=graph.length;
         boolean[] visited=new boolean[n];
         boolean[] dfsVisit=new boolean[n];
-       // Arrays.fill(safe,true);
         for(i=0;i<n;i++){
             if(!dfs(graph,i,visited,dfsVisit)){
                 ls.add(i);
