a179ee21cf8bc0aa5ee6a7f0d016040e985acf24
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-11 10:31:50 +0530

    Time: 13 ms (6.98%), Space: 42.6 MB (30.01%) - LeetHub

---
+++ 140-word-break-ii/140-word-break-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    ArrayList<String> ls=new ArrayList();
+    int n;
+    public void backTrack(String s,String t,int j){
+        if(j>=n){
+            //System.out.println(t);
+            ls.add(t.trim());
+            return;
+        }
+        String str="";
+        for(int i=j;i<n;i++){
+            str+=s.charAt(i);
+            if(set.contains(str)){
+                backTrack(s,t+str+" ",i+1);
+            }
+        }
+    }
+    HashSet<String> set;
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        n=s.length();
+        set=new HashSet();
+        for(String str:wordDict){
+            set.add(str);
+        }
+        backTrack(s,"",0);
+        return ls;
+    }
+}
