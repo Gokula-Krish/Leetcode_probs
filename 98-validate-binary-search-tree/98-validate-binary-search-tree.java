c974df7beaca5d02667e428930e0596709e1ffe4
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-11 07:40:21 +0530

    Time: 2 ms (23.55%), Space: 45 MB (5.89%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,28 +14,21 @@
  * }
  */
 class Solution {
-    public boolean isSorted(ArrayList<Integer> ls){
+    ArrayList<Integer> ls=new ArrayList();
+    public void inOrder(TreeNode root){
+        if(root==null){
+            return;
+        }inOrder(root.left);
+        ls.add(root.val);
+        inOrder(root.right);
+    }
+    public boolean isValidBST(TreeNode root) {
+        inOrder(root);
         for(int i=0;i<ls.size()-1;i++){
             if(ls.get(i)>=ls.get(i+1)){
                 return false;
             }
         }
         return true;
-    }
-    public void inOrder(TreeNode root,ArrayList<Integer> l){
-        if(root==null){
-            return;
-        }
-        inOrder(root.left,l);
-        l.add(root.val);
-        inOrder(root.right,l);
-    }
-    public boolean isValidBST(TreeNode root) {
-        if(root==null){
-            return true;
-        }
-        ArrayList<Integer> ls=new ArrayList<>();
-        inOrder(root,ls);
-        return isSorted(ls);
     }
 }
