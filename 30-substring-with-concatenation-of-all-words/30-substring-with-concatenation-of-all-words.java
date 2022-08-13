846de543878c1dc7354a79a2feec5c3a882cb6de
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-13 07:10:28 +0530

    Time: 390 ms (23.93%), Space: 169.2 MB (5.02%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -18,7 +18,8 @@ class Solution {
     }
     public boolean isValid(String str,HashMap<String,Integer> map){
         HashMap<String,Integer> map1=new HashMap();
-        for(int i=0;i<=(str.length()-sub);i+=sub){
+        int n1=str.length();
+        for(int i=0;i<=(n1-sub);i+=sub){
             String s=str.substring(i,i+sub);
             map1.put(s,map1.getOrDefault(s,0)+1);
         }      
