bb72b402705ed103d894454d906ed51f4de3f91a
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-13 20:52:21 +0530

    Time: 31 ms (67.24%), Space: 61.6 MB (14.13%) - LeetHub

---
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<TreeNode> ls=new ArrayList();
+    HashSet<String> set=new HashSet();
+    HashMap<String,TreeNode> map=new HashMap();
+    public String preOrder(TreeNode root,int l){
+        if(root==null){
+            return "N";
+        }
+        String left=preOrder(root.left,l+1);
+        String right=preOrder(root.right,l+1);
+        String str=root.val+","+left+","+right;
+       // System.out.println(str);
+        if(map.containsKey(str) && !set.contains(str)){
+         //   System.out.println(str+"--EQUAL--level "+l);
+            ls.add(root);
+            set.add(str);
+           // ls.add(map.get(str));
+        }
+        map.put(str,root);
+        return str;
+    }
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        preOrder(root,0);
+        return ls;
+    }
+}
