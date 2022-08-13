e88ef51b75d957a0310213b1af4c4036f067c700
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-13 20:23:33 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (84.93%) - LeetHub

--- 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.java
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.java
@@ -15,56 +15,59 @@
  */
 class Solution {
     TreeNode res=null;
-    public int postOrder(TreeNode root){
+    int max=0;
+    public int preOrder(TreeNode root,int level){
         if(root==null){
             return 0;
         }
-        int l=postOrder(root.left);
-        int r=postOrder(root.right);
-        if(l==r){
-            System.out.println(l+"----"+r+" and root is "+root.val);
+        int left=preOrder(root.left,level+1);
+        int right=preOrder(root.right,level+1);
+        if(left==right && left+level>=max){
             res=root;
+            max=left+level;
         }
-        return 1+Math.max(l,r);
+        return 1+Math.max(left,right);
     }
+    HashMap<TreeNode,TreeNode>  map=new HashMap();
     public TreeNode subtreeWithAllDeepest(TreeNode root) {
-        // int re=postOrder(root);
-        // System.out.println(re);
-        // return res;
-        Queue<TreeNode> q=new LinkedList();
-        q.add(root);
-        HashMap<TreeNode,TreeNode> map=new HashMap();
-        LinkedList<TreeNode> list=new LinkedList();
-        while(!q.isEmpty()){
-            int n=q.size();
-            list.clear();
-            for(int i=0;i<n;i++){
-                TreeNode c=q.poll();
-                list.add(c);
-                if(c.left!=null){
-                    q.add(c.left);
-                    map.put(c.left,c);
-                }
-                if(c.right!=null){
-                    q.add(c.right);
-                    map.put(c.right,c);
-                }
-            }
-        }
-        if(list.size()==1){
-            return list.get(0);
-        }
-        int j=0;
-        HashSet<TreeNode> set=new HashSet();
-        while(set.size()!=1){
-            set.clear();
-            for(int i=0;i<list.size();i++){
-                TreeNode node=map.get(list.get(i));
-                set.add(node);
-            }
-            list.clear();
-            list.addAll(set);
-        }
-        return set.iterator().next();
+        preOrder(root,0);
+        return res;
+       // System.out.println(re);
+       // return map.get(res);
+        // Queue<TreeNode> q=new LinkedList();
+        // q.add(root);
+        // map=new HashMap();
+        // LinkedList<TreeNode> list=new LinkedList();
+        // while(!q.isEmpty()){
+        //     int n=q.size();
+        //     list.clear();
+        //     for(int i=0;i<n;i++){
+        //         TreeNode c=q.poll();
+        //         list.add(c);
+        //         if(c.left!=null){
+        //             q.add(c.left);
+        //             map.put(c.left,c);
+        //         }
+        //         if(c.right!=null){
+        //             q.add(c.right);
+        //             map.put(c.right,c);
+        //         }
+        //     }
+        // }
+        // if(list.size()==1){
+        //     return list.get(0);
+        // }
+        // int j=0;
+        // HashSet<TreeNode> set=new HashSet();
+        // while(set.size()!=1){
+        //     set.clear();
+        //     for(int i=0;i<list.size();i++){
+        //         TreeNode node=map.get(list.get(i));
+        //         set.add(node);
+        //     }
+        //     list.clear();
+        //     list.addAll(set);
+        // }
+        // return set.iterator().next();
     }
 }
