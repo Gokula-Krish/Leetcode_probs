7c0c3da98bcde7856184d33e3e124e2e7744b319
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-20 09:46:44 +0530

    Time: 9 ms (52.71%), Space: 45 MB (43.43%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -1,25 +1,40 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        Stack<Character> stack=new Stack();
+        // Stack<Character> stack=new Stack();
+        // int n=part.length();
+        // for(char c:s.toCharArray()){
+        //     stack.push(c);
+        //     if(stack.size()>=n){
+        //         int i=n-1;
+        //         Stack<Character> stack1=new Stack();
+        //         stack1.addAll(stack);
+        //         while(!stack.empty() && i>=0 && stack.peek()==part.charAt(i)){
+        //             stack.pop();i--;
+        //         }
+        //         if(i!=-1){
+        //             stack=stack1;
+        //         }
+        //     }
+        // }
+        // StringBuilder sb=new StringBuilder();
+        // while(!stack.empty()){
+        //     sb.append(stack.pop());
+        // }
+        return removeAll(s,part);
+    }
+    public String removeAll(String s,String part){
+        StringBuilder sb=new StringBuilder();
         int n=part.length();
         for(char c:s.toCharArray()){
-            stack.push(c);
-            if(stack.size()>=n){
-                int i=n-1;
-                Stack<Character> stack1=new Stack();
-                stack1.addAll(stack);
-                while(!stack.empty() && i>=0 && stack.peek()==part.charAt(i)){
-                    stack.pop();i--;
-                }
-                if(i!=-1){
-                    stack=stack1;
+            sb.append(c);
+            if(sb.length()>=n && (sb.substring(sb.length()-n).toString().equals(part))){
+                int i=0;
+                while(i<n){
+                    sb.deleteCharAt(sb.length()-1);
+                    i++;
                 }
             }
         }
-        StringBuilder sb=new StringBuilder();
-        while(!stack.empty()){
-            sb.append(stack.pop());
-        }
-        return new String(sb.reverse());
+        return sb.toString();
     }
 }
