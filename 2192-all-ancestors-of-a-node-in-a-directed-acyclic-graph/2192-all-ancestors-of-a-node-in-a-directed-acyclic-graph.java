7006cad51d564cccd441916ae65bbf0c36aedf1a
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-26 23:32:07 +0530

    Time: 321 ms (22.18%), Space: 135.4 MB (50.84%) - LeetHub

---
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -0,0 +1,34 @@
+class Solution {
+    TreeSet<Integer> set;
+    boolean[] dfsVisit;
+    public List<List<Integer>> getAncestors(int n, int[][] edges) {
+        LinkedList<Integer> list[]=new LinkedList[n];
+        List<List<Integer>> ls=new ArrayList();
+        for(int i=0;i<n;i++){
+            list[i]=new LinkedList();
+        }
+        for(int[] arr:edges){
+            list[arr[1]].add(arr[0]);
+        }
+        
+        for(int i=0;i<n;i++){
+            set=new TreeSet();
+            dfsVisit=new boolean[n];
+            dfs(list,i);
+            ls.add(new ArrayList(set));
+          //  System.out.println(set);
+        }
+        return ls;
+    }
+    public void dfs(LinkedList<Integer> list[],int j){
+        dfsVisit[j]=true;
+      ///  if(!(set.size()==0)){set.add(j);}
+        for(int i:list[j]){
+            if(!dfsVisit[i]){
+                set.add(i);
+                dfs(list,i);
+            }
+        }
+       // dfsVisit[j]=false;
+    }
+}
