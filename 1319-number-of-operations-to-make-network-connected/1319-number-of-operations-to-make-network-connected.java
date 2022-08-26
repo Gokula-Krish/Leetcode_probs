7cddd5787e30e3e3a39126ef7e8cf4b116121fad
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-26 07:04:37 +0530

    Time: 25 ms (52.66%), Space: 77.5 MB (21.15%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        int wires=connections.length;
+        if(wires<(n-1)){
+            return -1;
+        }
+        LinkedList<Integer> list[]=new LinkedList[n];
+        for(int i=0;i<n;i++){
+            list[i]=new LinkedList<Integer>();
+        }
+        for(int[] a:connections){
+            list[a[0]].add(a[1]);
+            list[a[1]].add(a[0]);
+        }
+        boolean[] visited=new boolean[n];
+      //  Arrays.fill(visited,false);
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(list,i,visited);
+                c++;
+            }
+        }
+        return c-1;
+    }
+    public void dfs(LinkedList<Integer> list[],int j,boolean[] visited){
+        visited[j]=true;
+        for(int i:list[j]){
+            if(!visited[i]){
+                dfs(list,i,visited);
+            }
+        }
+    }
+}
