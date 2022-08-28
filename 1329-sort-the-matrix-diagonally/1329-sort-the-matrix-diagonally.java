d36f96770fa3dea89016c2b02cb1cd554e114fcd
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-28 06:09:45 +0530

    Time: 7 ms (80.21%), Space: 43.3 MB (91.41%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -2,39 +2,6 @@ class Solution {
     public int[][] diagonalSort(int[][] mat) {
         int m=mat.length,n=mat[0].length;
         HashMap<Integer,PriorityQueue<Integer>> map=new HashMap();
-        // int l=0;
-        // for(int i=0;i<mat[0].length;i++){
-        //     map.put(i,new PriorityQueue());
-        //     int k=0,j=i;
-        //     while(k<mat.length && j<mat[0].length){
-        //         map.get(i).add(mat[k][j]);
-        //         k++;j++;
-        //     }
-        // }
-        // l=-1;
-        // for(int i=1;i<mat.length;i++){
-        //     map.put(l,new PriorityQueue());
-        //     int k=i,j=0;
-        //     while(k<mat.length && j<mat[0].length){
-        //         map.get(l).add(mat[k][j]);
-        //         k++;j++;
-        //     }l--;
-        // }
-        // for(int i=0;i<mat[0].length;i++){
-        //     int k=0,j=i;
-        //     while(k<mat.length && j<mat[0].length){
-        //         mat[k][j]=map.get(i).poll();
-        //         k++;j++;
-        //     }
-        // }
-        // l=-1;
-        // for(int i=1;i<mat.length;i++){
-        //     int k=i,j=0;
-        //     while(k<mat.length && j<mat[0].length){
-        //         mat[k][j]=map.get(l).poll();
-        //         k++;j++;
-        //     }l--;
-        // }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 int key=i-j;
