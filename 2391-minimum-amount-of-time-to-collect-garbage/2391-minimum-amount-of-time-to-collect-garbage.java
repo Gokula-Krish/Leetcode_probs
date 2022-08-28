54e5983ef7761647f78948b60b245b4da0d9e48b
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-28 12:41:55 +0530

    Time: 45 ms (33.33%), Space: 58.9 MB (100.00%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -4,16 +4,15 @@ class Solution {
         for(int i=1;i<travel.length;i++){
             travel[i]+=travel[i-1];
         }
-        for(String s:garbage){
-            sum+=s.length();
-        }
         HashMap<Character,Integer> map=new HashMap();
-        int i=0;
+        int i=0;int count=0;
         for(String s:garbage){
             for(char c:s.toCharArray()){
                 map.put(c,i);
+                count++;
             }i++;
         }
+        sum+=count;
         for(Map.Entry<Character,Integer> e:map.entrySet()){
             int ans=e.getValue();
             if(ans!=0)sum+=travel[ans-1];
