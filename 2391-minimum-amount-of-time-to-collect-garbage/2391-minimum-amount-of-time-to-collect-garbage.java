98bed548702c9d358b903eaf874b370b0b900fe8
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-28 12:40:37 +0530

    Time: 73 ms (33.33%), Space: 108.3 MB (33.33%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int garbageCollection(String[] garbage, int[] travel) {
+        int sum=0,j=0;
+        for(int i=1;i<travel.length;i++){
+            travel[i]+=travel[i-1];
+        }
+        for(String s:garbage){
+            sum+=s.length();
+        }
+        HashMap<Character,Integer> map=new HashMap();
+        int i=0;
+        for(String s:garbage){
+            for(char c:s.toCharArray()){
+                map.put(c,i);
+            }i++;
+        }
+        for(Map.Entry<Character,Integer> e:map.entrySet()){
+            int ans=e.getValue();
+            if(ans!=0)sum+=travel[ans-1];
+        }
+        return sum;
+    }
+}
