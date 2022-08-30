d8e85d5b47000887fbecbeeb4f8019444d4b694e
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-08-30 21:48:18 +0530

    Time: 7 ms (30.99%), Space: 43.2 MB (48.43%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void preOrder(TreeNode root,int row,int col){
+        if(root==null){
+            return;
+        }
+        if(map.containsKey(col)){
+            if(map.get(col).containsKey(row)){
+                map.get(col).get(row).add(root.val);
+            }
+            else{
+                map.get(col).put(row,new PriorityQueue());
+                map.get(col).get(row).add(root.val);
+            }
+        }
+        else{
+            map.put(col,new TreeMap<Integer,PriorityQueue<Integer>>());
+            map.get(col).put(row,new PriorityQueue());
+            map.get(col).get(row).add(root.val);
+        }
+        preOrder(root.left,row+1,col-1);
+        preOrder(root.right,row+1,col+1);
+    }
+    TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map=new TreeMap();
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> ls=new ArrayList();
+        preOrder(root,0,0);
+     //   System.out.println(map);
+        for(Map.Entry<Integer,TreeMap<Integer,PriorityQueue<Integer>>> e:map.entrySet()){
+            List<Integer> l=new ArrayList();
+           // System.out.println(e.getValue());
+            TreeMap<Integer,PriorityQueue<Integer>> m1=e.getValue();
+            for(Map.Entry<Integer,PriorityQueue<Integer>> e1:m1.entrySet()){
+                PriorityQueue<Integer> p1=e1.getValue();
+                while(!p1.isEmpty()){
+                    l.add(p1.poll());
+                }
+              //  ls.addAll(e1.getValue());
+            }
+            ls.add(l);
+        }
+        return ls;
+    }
+}
