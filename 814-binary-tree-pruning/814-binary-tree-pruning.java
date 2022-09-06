2d0694494e989203672cd6dd7898254632e334ed
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-09-06 21:26:27 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (58.05%) - LeetHub

---
+++ 814-binary-tree-pruning/814-binary-tree-pruning.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean contains(TreeNode root){
+        if(root==null){
+            return false;
+        }
+        boolean left=contains(root.left);
+        boolean right=contains(root.right);
+        if(!left)root.left=null;
+        if(!right)root.right=null;
+        return root.val==1 || left || right;
+    }
+    public TreeNode pruneTree(TreeNode root) {
+        if(!contains(root)){
+            return null;
+        }
+        return root;
+    }
+}
