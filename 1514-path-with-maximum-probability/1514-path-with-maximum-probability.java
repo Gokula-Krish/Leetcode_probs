fceff7376fbf40260131a55d92903a60dac4af59
Gokula-Krish <64589823+Gokula-Krish@users.noreply.github.com>
2022-09-10 17:09:58 +0530

    Time: 95 ms (40.09%), Space: 81.6 MB (49.16%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -0,0 +1,29 @@
+class Solution {
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        LinkedList<double[]> G[]=new LinkedList[n];
+        for(int i=0;i<n;i++){
+            G[i]=new LinkedList<double[]>();
+        }
+        int i=0;
+        for(int[] arr:edges){
+            G[arr[0]].add(new double[]{arr[1],succProb[i]});
+            G[arr[1]].add(new double[]{arr[0],succProb[i]});
+            i++;
+        }
+        PriorityQueue<double[]> pq=new PriorityQueue<double[]>((a,b)->(Double.compare(b[1],a[1])));
+        pq.add(new double[]{start,1});
+        double[] d=new double[n];
+        Arrays.fill(d,Integer.MIN_VALUE);
+        d[start]=(double)1;
+        while(!pq.isEmpty()){
+            double[] node=pq.poll();
+            for(double[] arr:G[(int)node[0]]){
+                if(d[(int)node[0]]*arr[1]>d[(int)arr[0]]){
+                    d[(int)arr[0]]=d[(int)node[0]]*arr[1];
+                    pq.add(new double[]{arr[0],d[(int)arr[0]]});
+                }
+            }
+        }
+        return d[end]==Integer.MIN_VALUE?0:d[end];
+    }
+}
